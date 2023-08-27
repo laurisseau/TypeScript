e329067be19cf17fcc98f074aa7f6b5314dd5a4a
laurisseau Joseph <reso0208@gmail.com>
2023-08-26 21:54:52 -0400

    8/26/23

--- client/app/server.ts
+++ client/app/server.ts
@@ -41,7 +41,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'https://typescript-messenger-80b8feb4ad1c.herokuapp.com/',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST'], // You can adjust the allowed methods
   })
 );
